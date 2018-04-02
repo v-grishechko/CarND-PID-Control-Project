98189184533d37565772b345af601961c8253ec7
v-grishechko <vladislav.grishechco@gmail.com>
2018-04-02 21:31:44 +0300

    Final settings

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,7 +1,5 @@
 #include "PID.h"
 
-using namespace std;
-
 /*
 * TODO: Complete the PID class.
 */
@@ -11,11 +9,23 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    prev_cte = 0;
 }
 
 void PID::UpdateError(double cte) {
+    integral += cte;
+
+    p_error = -Kp * cte;
+    d_error = -Kd * (cte - prev_cte);
+    i_error = -Ki * integral;
+    prev_cte = cte;
 }
 
+
 double PID::TotalError() {
+    return p_error + d_error + i_error;
 }
 
